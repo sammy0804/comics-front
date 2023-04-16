baa2ebbad34e27e4bde265b3c5c263f8eb160b8a
sammy0804 <sami31rincon@gmail.com>
2023-04-16 02:27:53 -0500

    Saved score and get list of comic with score

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import { useEffect, useState } from 'react';
-import { getComic } from './data/Api.js';
+import { getComic,getRatedComics,postRateComic } from './data/Api.js';
 import {AiOutlineStar, AiFillStar} from 'react-icons/ai';
 
 function App() {
   const [comic,setComic] = useState(null);
   const [score, setScore] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [ratedComics, setRatedComics] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+  const handleClose = () => setShowModal(false);
   const MILLISECONDS_PER_DAY = 600000; 
 
   const ObtainComic = async()=>{
@@ -19,6 +23,11 @@ function App() {
 
   const handleScore = (value) => {
     setScore(value);
+    postRateComic(comic.num, value);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
   };
 
   const Stars = ()=>{
@@ -49,6 +58,16 @@ function App() {
     window.location.reload();
   }, MILLISECONDS_PER_DAY);
 
+  const toggleModal = async () => {
+    setShowModal(!showModal);
+    const response = await getRatedComics();
+    console.log(JSON.stringify(response));
+
+    if (response.status === 200) {
+      setRatedComics(response.comics);
+    }
+  };
+
   if(comic == null){
     return(
       <main className='App'>
@@ -68,6 +87,39 @@ function App() {
         <div className='card-footer'>
           <Stars/>
         </div>
+        <div className='card-button'>
+          <button type='button' className='btn btn-primary' onClick={toggleModal}>Ver Cómics Calificados</button>
+        </div>
+        {showAlert && (
+          <div className='alert alert-success' role='alert' onClose={()=>setShowAlert(false)}>
+            Comic calificado exitosamente!
+          </div>
+        )}
+        {showModal && (
+        <div className="comic-modal">
+          <div className="comic-modal-dialog">
+            <div className="comic-modal-content">
+              <div className="comic-modal-header">
+                <h5 className="comic-modal-title">Comics List</h5>
+              </div>
+              <div className="comic-modal-body">
+                <ul>
+                  {ratedComics.map((comic,index) => (
+                    <li key={index}>
+                      {comic.comic.title} (ID: {comic.comic.num}) - Rating: {comic.rate}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="comic-modal-footer">
+                <button className="btn btn-secondary" onClick={handleClose}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        )}
       </article>
     </main>
   );
--- src/data/Api.js
+++ src/data/Api.js
@@ -1,5 +1,5 @@
 export async function getComic() {
-  const url = "http://localhost:5190/comics";
+  const url = "http://localhost:5000/comics";
   
   try {
     const response = await window.fetch(url);
@@ -9,5 +9,29 @@ export async function getComic() {
     console.log(error);
     return { status: error.response.status, comics: null };
   }
+  
+}
+
+export async function getRatedComics() {
+  const url = "http://localhost:5000/comics/rated";
+  
+  try {
+    const response = await window.fetch(url);
+    const body = await response.json();
+    return { status: response.status, comics: body };
+  } catch (error) {
+    console.log(error);
+    return { status: error.response.status, comics: null };
+  }
+  
+}
+
+export async function postRateComic(id, rate) {
+  const url = "http://localhost:5000/comics?id="+id+"&rate="+rate;
+  
+  const response = await window.fetch(url,{
+    method:'POST',
+    headers:{'Content-Type': 'application/json'}
+  });
   
 }
